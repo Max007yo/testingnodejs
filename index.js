fe3d5dd0b0896eb1a71fd9c2352dcb5508399a03
max <gboy9973@gmail.com>
2021-09-14 11:40:55 +0530

    test 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 const download_url = 'https://dl3.downloadly.ir/Files/Elearning/Udemy_Build_Your_Own_First_Person_Shooter_Survival_Game_in_Unity_2019-8.part1_Downloadly.ir.rar';
 
