c327df56ef468c1d219b64ce52a22e85485cd67f
max <gboy9973@gmail.com>
2021-09-14 11:36:39 +0530

    test 2

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT | 3000;
 
 const download_url = 'https://dl3.downloadly.ir/Files/Elearning/Udemy_Build_Your_Own_First_Person_Shooter_Survival_Game_in_Unity_2019-8.part1_Downloadly.ir.rar';
 
-app.get('*', async (req, res) => {
+app.get('/', (req, res) => {
+    res.end('hello');
+});
+
+app.get('/test', async (req, res) => {
     const r = await axios.head(download_url);
     res.end(JSON.stringify(r.headers));
 });
