15f0db6135e6245c375448b88d2f7e8f39cda84b
max <gboy9973@gmail.com>
2021-09-20 15:38:53 +0530

    improved log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { Storage } = require('megajs');
 const axios = require('axios').default;
 
 const express = require('express');
+const { randomUUID } = require('crypto');
 const app = express();
 
 app.use(express.json());
@@ -14,30 +15,31 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload-to-mega/', (req, res, next) => {
+    const clientId = randomUUID();
     const storage = new Storage({
         email: req.body.email,
         password: req.body.password,
     }, async err => {
         if(!err) {
-            console.log('Connected to mega with no errors');
+            console.log(`Client ${clientId} Connected to Mega cloud with no errors`);
             try {
-                
-                console.log('Request sent');
+
+                console.log(`Client ${clientId} Request sent to ${req.body.fileUrl}`);
                 const r = await axios.get(req.body.fileUrl, { responseType: 'stream' });
-                console.log('Got response');
+                console.log(`Client ${clientId} Got response from ${req.body.fileUrl}`);
 
                 const fileSize = parseInt(r.headers['content-length']);
                 const upload = storage.upload({ name: path.basename(req.body.fileUrl), size: fileSize });
 
-                upload.on('close', () => console.log('Upload Finished'));
-                upload.on('error', err => console.log('Upload ' + err));
+                upload.on('close', () => console.log(`Client ${clientId} Upload Finished`));
+                upload.on('error', err => console.log(`Client ${clientId} Upload ` + err));
 
-                r.data.on('end', () => console.log('Download Complete'));
-                r.data.on('error', err => console.log('Download ' + err));
+                r.data.on('end', () => console.log(`Client ${clientId} Download Complete`));
+                r.data.on('error', err => console.log(`Client ${clientId} Download ` + err));
 
                 r.data.pipe(upload);
 
-                console.log('Uploading...');
+                console.log(`Client ${clientId} Uploading...`);
                 res.end('Uploading...');
 
             } catch(e) { 
