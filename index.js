9d532253f334952c9fc99db282722b23a385762f
max <gboy9973@gmail.com>
2021-09-20 15:15:58 +0530

    added error handeling

--- index.js
+++ index.js
@@ -19,18 +19,27 @@ app.post('/upload-to-mega/', (req, res, next) => {
         password: req.body.password,
     }, async err => {
         if(!err) {
-            console.log('connected to mega with no errors');
+            console.log('Connected to mega with no errors');
             try {
-                console.log('sending request...');
+                
+                console.log('Request sent');
                 const r = await axios.get(req.body.fileUrl, { responseType: 'stream' });
-                console.log('got response');
-                storage.upload(path.basename(req.body.fileUrl), r.data,
-                    err => {
-                        if(!err) console.log('upload complete');
-                        else console.log(err);
-                });
-                console.log('uploading...');
-                res.end('uploading...');
+                console.log('Got response');
+
+                const fileSize = parseInt(r.headers['content-length']);
+                const upload = storage.upload({ name: path.basename(req.body.fileUrl), size: fileSize });
+
+                upload.on('close', () => console.log('Upload Finished'));
+                upload.on('error', err => console.log('Upload ' + err));
+
+                r.data.on('end', () => console.log('Download Complete'));
+                r.data.on('error', err => console.log('Download ' + err));
+
+                r.data.pipe(upload);
+
+                console.log('Uploading...');
+                res.end('Uploading...');
+
             } catch(e) { 
                 console.log(e);
                 res.end(e.message);
