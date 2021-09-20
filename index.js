a19477b17f7308ce454c63422f06d2f6161da3da
max <gboy9973@gmail.com>
2021-09-20 16:24:04 +0530

    added download progress

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { Storage } = require('megajs');
 const axios = require('axios').default;
 
 const express = require('express');
-const { randomUUID } = require('crypto');
 const app = express();
 
 app.use(express.json());
@@ -15,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload-to-mega/', (req, res, next) => {
-    const clientId = randomUUID();
+    const clientId = makeId(8);
     const storage = new Storage({
         email: req.body.email,
         password: req.body.password,
@@ -30,6 +29,7 @@ app.post('/upload-to-mega/', (req, res, next) => {
 
                 const fileSize = parseInt(r.headers['content-length']);
                 const upload = storage.upload({ name: path.basename(req.body.fileUrl), size: fileSize });
+                r.data.pipe(upload);
 
                 upload.on('close', () => console.log(`Client ${clientId} Upload Finished`));
                 upload.on('error', err => console.log(`Client ${clientId} Upload ` + err));
@@ -37,7 +37,13 @@ app.post('/upload-to-mega/', (req, res, next) => {
                 r.data.on('end', () => console.log(`Client ${clientId} Download Complete`));
                 r.data.on('error', err => console.log(`Client ${clientId} Download ` + err));
 
-                r.data.pipe(upload);
+                let downloadedMB = 0;
+                const fileSizeInMB = (fileSize / (1024 * 1024)).toFixed(2);
+                r.data.on('data', chunk => downloadedMB += (chunk.length / (1024 * 1024)));
+                const progressTimer = setInterval(() => {
+                    console.log(`Client ${clientId} ${downloadedMB.toFixed(2)}/${fileSizeInMB}MB Downloaded`);
+                    if(downloadedMB.toFixed(2) === fileSizeInMB) clearInterval(progressTimer);
+                }, 2 * 1000);
 
                 console.log(`Client ${clientId} Uploading...`);
                 res.end('Uploading...');
@@ -56,3 +62,11 @@ app.post('/upload-to-mega/', (req, res, next) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is listening on PORT ${PORT}...`));
 
+function makeId(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    } return result;
+}
